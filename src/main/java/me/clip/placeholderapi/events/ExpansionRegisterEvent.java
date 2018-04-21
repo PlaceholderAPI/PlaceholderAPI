796136982afc207a88f1daa385eede494b2dd908
extendedclip <fairviewent@yahoo.com>
2018-04-20 22:07:03 -0400

    Added ExpansionRegisterEvent

---
+++ src/main/java/me/clip/placeholderapi/events/ExpansionRegisterEvent.java
@@ -0,0 +1,48 @@
+/*
+ *
+ * PlaceholderAPI
+ * Copyright (C) 2018 Ryan McCarthy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package me.clip.placeholderapi.events;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class ExpansionRegisterEvent extends Event {
+
+	private static final HandlerList HANDLERS = new HandlerList();
+	private PlaceholderExpansion expansion;
+
+	public ExpansionRegisterEvent(PlaceholderExpansion expansion) {
+		this.expansion = expansion;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return HANDLERS;
+	}
+
+	public static HandlerList getHandlerList() {
+		return HANDLERS;
+	}
+	
+	public PlaceholderExpansion getExpansion() {
+		return expansion;
+	}
+}
