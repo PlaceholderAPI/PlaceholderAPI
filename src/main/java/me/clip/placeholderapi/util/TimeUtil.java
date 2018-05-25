a475fc0f803eb603ab8239df9ee9a76d5895e87c
Praya Amadiga Pitasa <31617878+PrayaAmadigaPitasa@users.noreply.github.com>
2018-05-26 00:25:50 +0700

    Change code for get time

--- src/main/java/me/clip/placeholderapi/util/TimeUtil.java
+++ src/main/java/me/clip/placeholderapi/util/TimeUtil.java
@@ -20,143 +20,60 @@
  */
 package me.clip.placeholderapi.util;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.clip.placeholderapi.PlaceholderAPIPlugin;
+import me.clip.placeholderapi.configuration.PlaceholderAPIConfig;
+import me.clip.placeholderapi.util.TimeFormat;
+
 public class TimeUtil {
-
-	public static String getRemaining(int seconds, TimeFormat type) {
-		
-		if (seconds < 60) {
-			switch(type) {
-			case DAYS:
-				return "0";
-			case HOURS:
-				return "0";
-			case MINUTES:
-				return "0";
-			case SECONDS:
-				return String.valueOf(seconds);
-			}
-			return String.valueOf(seconds);
-		}
-		
-		int minutes = seconds / 60;
-		int s = 60 * minutes;
-		int secondsLeft = seconds - s;
-		if (minutes < 60) {
-			switch(type) {
-			case DAYS:
-				return "0";
-			case HOURS:
-				return "0";
-			case MINUTES:
-				return String.valueOf(minutes);
-			case SECONDS:
-				return String.valueOf(secondsLeft);
-			}
-			return String.valueOf(seconds);
-		}
-		
-		if (minutes < 1440) {
-			int hours = minutes / 60;
-			int inMins = 60 * hours;
-			int leftOver = minutes - inMins;
-			switch(type) {
-			case DAYS:
-				return "0";
-			case HOURS:
-				return String.valueOf(hours);
-			case MINUTES:
-				return String.valueOf(leftOver);
-			case SECONDS:
-				return String.valueOf(secondsLeft);
-			}
-			return String.valueOf(seconds);
-		}
-		
-		int days = minutes / 1440;
-		int inMins = 1440 * days;
-		int leftOver = minutes - inMins;
-			
-		if (leftOver < 60) {
-			switch (type) {
-			case DAYS:
-				return String.valueOf(days);
-			case HOURS:
-				return String.valueOf(0);
-			case MINUTES:
-				return String.valueOf(leftOver);
-			case SECONDS:
-				return String.valueOf(secondsLeft);
-			}
-			return String.valueOf(seconds);
-
-		} else {
-			int hours = leftOver / 60;
-			int hoursInMins = 60 * hours;
-			int minsLeft = leftOver - hoursInMins;
-			switch (type) {
-			case DAYS:
-				return String.valueOf(days);
-			case HOURS:
-				return String.valueOf(hours);
-			case MINUTES:
-				return String.valueOf(minsLeft);
-			case SECONDS:
-				return String.valueOf(secondsLeft);
-			}
-			return String.valueOf(seconds);
-		}	
-	}
 	
-	public static String getTime(int seconds) {
+	public static final String getRemaining(int seconds, TimeFormat type) {
+		final PlaceholderAPIPlugin plugin = (PlaceholderAPIPlugin) JavaPlugin.getProvidingPlugin(PlaceholderAPIPlugin.class);
+		final PlaceholderAPIConfig config = plugin.getPlaceholderAPIConfig();
+		final String time = config.getTime(type);
+		final int days = seconds / 86400; seconds = seconds - (days * 86400);
+		final int hours = seconds / 3600; seconds = seconds - (hours * 3600);
+		final int minutes = seconds / 60; seconds = seconds - (minutes * 60);
 		
-		if (seconds < 60) {
-			return seconds+"s";
+		switch (type) {
+		case DAYS : return days + time;
+		case HOURS : return hours + time;
+		case MINUTES : return minutes + time;
+		case SECONDS : return seconds + time;
+		default : return null;
+		}
+	}
+
+	public static final String getTime(int seconds) {
+		final PlaceholderAPIPlugin plugin = (PlaceholderAPIPlugin) JavaPlugin.getProvidingPlugin(PlaceholderAPIPlugin.class);
+		final PlaceholderAPIConfig config = plugin.getPlaceholderAPIConfig();
+		final String time = config.getTime(type);
+		final String timeDays = getTime(TimeFormat.DAYS);
+		final String timeHours = getTime(TimeFormat.HOURS);
+		final String timeMinutes = getTime(TimeFormat.MINUTES);
+		final String timeSeconds = getTime(TimeFormat.SECONDS);
+		final StringBuilder timeBuilder = new StringBuilder();
+		final int days = seconds / 86400; seconds = seconds - (days * 86400);
+		final int hours = seconds / 3600; seconds = seconds - (hours * 3600);
+		final int minutes = seconds / 60; seconds = seconds - (minutes * 60);
+		
+		if (days > 0) {
+			timeBuilder.append(days).append(timeDays).append(" ");
+			timeBuilder.append(hours).append(timeHours).append(" ");
+			timeBuilder.append(minutes).append(timeMinutes).append(" ");
+			timeBuilder.append(seconds).append(timeSeconds);
+		} else if (hours > 0) {
+			timeBuilder.append(hours).append(timeHours).append(" ");
+			timeBuilder.append(minutes).append(timeMinutes).append(" ");
+			timeBuilder.append(seconds).append(timeSeconds);
+		} else if (minutes > 0) {
+			timeBuilder.append(minutes).append(timeMinutes).append(" ");
+			timeBuilder.append(seconds).append(timeSeconds);
+		} else {
+			timeBuilder.append(seconds).append(timeSeconds);
 		}
 		
-		int minutes = seconds / 60;
-		int s = 60 * minutes;
-		int secondsLeft = seconds - s;
-		if (minutes < 60) {
-			if (secondsLeft > 0) {
-				return String.valueOf(minutes+"m "+secondsLeft+"s");
-			} else {
-				return String.valueOf(minutes+"m");
-			}
-		}
-		if (minutes < 1440) {
-			String time;
-			int hours = minutes / 60;
-			time = hours+"h";
-			int inMins = 60 * hours;
-			int leftOver = minutes - inMins;
-			if (leftOver >= 1) {
-				time = time+" "+leftOver+"m";
-			}
-			if (secondsLeft > 0) {
-				time = time+" "+secondsLeft+"s";
-			}
-			return time;
-		}
-		
-		String time;
-		int days = minutes / 1440;
-		time = days+"d";
-		int inMins = 1440 * days;
-		int leftOver = minutes - inMins;
-		if (leftOver >= 1) {
-			if (leftOver < 60) {
-				time = time+" "+leftOver+"m";
-			} else {
-				int hours = leftOver / 60;
-				time = time+" "+hours+"h";
-				int hoursInMins = 60 * hours;
-				int minsLeft = leftOver - hoursInMins;
-				time = time+" "+minsLeft+"m";
-			}
-		}
-		if (secondsLeft > 0) {
-			time = time+" "+secondsLeft+"s";
-		}
-		return time;
+		return timeBuilder.toString();
 	}
 }
