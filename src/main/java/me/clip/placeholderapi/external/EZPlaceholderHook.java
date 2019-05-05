75c3027e055867f9b1f1f529b743db679f5668e6
extendedclip <fairviewent@yahoo.com>
2019-05-05 13:59:40 -0400

    This will not be around for much longer. Plugin authors need to update to use PlaceholderExpansion rather than this class.

---
+++ src/main/java/me/clip/placeholderapi/external/EZPlaceholderHook.java
@@ -0,0 +1,57 @@
+/*
+ *
+ * PlaceholderAPI
+ * Copyright (C) 2018 Ryan McCarthy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package me.clip.placeholderapi.external;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.PlaceholderHook;
+import org.apache.commons.lang.Validate;
+import org.bukkit.plugin.Plugin;
+
+@Deprecated
+public abstract class EZPlaceholderHook extends PlaceholderHook {
+
+  private String identifier;
+
+  private String plugin;
+
+  public EZPlaceholderHook(Plugin plugin, String identifier) {
+    Validate.notNull(plugin, "Plugin can not be null!");
+    Validate.notNull(identifier, "Placeholder name can not be null!");
+    this.identifier = identifier;
+    this.plugin = plugin.getName();
+  }
+
+  public boolean isHooked() {
+    return PlaceholderAPI.getRegisteredPlaceholderPlugins().contains(identifier);
+  }
+
+  public boolean hook() {
+    return PlaceholderAPI.registerPlaceholderHook(identifier, this);
+  }
+
+  public String getPlaceholderName() {
+    return identifier;
+  }
+
+  public String getPluginName() {
+    return plugin;
+  }
+}
