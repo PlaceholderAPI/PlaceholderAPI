0bd0790e72de57566ecc24d1794e786f34e71552
Garrett <gmatrixgames@hotmail.com>
2019-10-24 16:55:42 -0300

    Fix copyright header to fit the rest

--- bukkit/src/main/java/me/clip/placeholderapi/bukkit/PlaceholderListener.java
+++ bukkit/src/main/java/me/clip/placeholderapi/bukkit/PlaceholderListener.java
@@ -1,5 +1,4 @@
 /*
- *
  * PlaceholderAPI
  * Copyright (C) 2019 Ryan McCarthy
  *
@@ -15,8 +14,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- *
  */
 package me.clip.placeholderapi.bukkit;
 
