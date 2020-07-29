a78b5fe7124be6c4df5698ea33b219216ac39479
Sxtanna <ranald.taylor@gmail.com>
2020-07-29 01:49:41 -0400

    updated to use Player#spigot instead of CommandSender#spigot (I miss Kotlin)

--- src/main/java/me/clip/placeholderapi/commands/impl/local/CommandParse.java
+++ src/main/java/me/clip/placeholderapi/commands/impl/local/CommandParse.java
@@ -139,7 +139,7 @@ public final class CommandParse extends PlaceholderCommand
 			}
 			else
 			{
-				sender.spigot().sendMessage(TextComponent.fromLegacyText(message));
+				((Player) sender).spigot().sendMessage(TextComponent.fromLegacyText(message));
 			}
 		}
 	}
