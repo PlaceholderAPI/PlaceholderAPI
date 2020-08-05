ccb0342098057b19479120eb07de27c918d43585
Fefo6644 <federico.lopez.1999@outlook.com>
2020-08-05 17:22:25 -0300

    Refactored `TimeUtil` utility class.
    * Minimized `TimeUtil#getRemaining`.
    * Fixed a bug regarding how days and hours were calculated in `TimeUtil#getTime`.
    * Ditched the use of `int`s and use `long`s instead.

--- src/main/java/me/clip/placeholderapi/util/TimeUtil.java
+++ src/main/java/me/clip/placeholderapi/util/TimeUtil.java
@@ -22,100 +22,27 @@ package me.clip.placeholderapi.util;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.StringJoiner;
 
 public class TimeUtil {
 
-  public static String getRemaining(int seconds, TimeFormat type) {
-    if (seconds < 60) {
-      switch (type) {
-        case DAYS:
-        case HOURS:
-        case MINUTES:
-          return "0";
-        case SECONDS:
-          return String.valueOf(seconds);
-      }
+  public static String getRemaining(final long seconds, final TimeFormat type) {
+    switch (type) {
+      default:
+        return String.valueOf(seconds);
 
-      return String.valueOf(seconds);
+      case SECONDS:
+        return String.valueOf(seconds % 60);
+
+      case MINUTES:
+        return String.valueOf((seconds / 60) % 60);
+
+      case HOURS:
+        return String.valueOf((seconds / 3600) % 24);
+
+      case DAYS:
+        return String.valueOf(seconds / 86400);
     }
-
-    int minutes = seconds / 60;
-    int s = 60 * minutes;
-    int secondsLeft = seconds - s;
-
-    if (minutes < 60) {
-      switch (type) {
-        case DAYS:
-        case HOURS:
-          return "0";
-        case MINUTES:
-          return String.valueOf(minutes);
-        case SECONDS:
-          return String.valueOf(secondsLeft);
-      }
-
-      return String.valueOf(seconds);
-    }
-
-    if (minutes < 1440) {
-      int hours = minutes / 60;
-      int inMins = 60 * hours;
-      int leftOver = minutes - inMins;
-
-      switch (type) {
-        case DAYS:
-          return "0";
-        case HOURS:
-          return String.valueOf(hours);
-        case MINUTES:
-          return String.valueOf(leftOver);
-        case SECONDS:
-          return String.valueOf(secondsLeft);
-      }
-
-      return String.valueOf(seconds);
-    }
-
-    int days = minutes / 1440;
-    int inMins = 1440 * days;
-    int leftOver = minutes - inMins;
-
-    if (leftOver < 60) {
-      switch (type) {
-        case DAYS:
-          return String.valueOf(days);
-        case HOURS:
-          return String.valueOf(0);
-        case MINUTES:
-          return String.valueOf(leftOver);
-        case SECONDS:
-          return String.valueOf(secondsLeft);
-      }
-
-      return String.valueOf(seconds);
-
-    } else {
-      int hours = leftOver / 60;
-      int hoursInMins = 60 * hours;
-      int minsLeft = leftOver - hoursInMins;
-
-      switch (type) {
-        case DAYS:
-          return String.valueOf(days);
-        case HOURS:
-          return String.valueOf(hours);
-        case MINUTES:
-          return String.valueOf(minsLeft);
-        case SECONDS:
-          return String.valueOf(secondsLeft);
-      }
-
-      return String.valueOf(seconds);
-    }
-  }
-
-  public static String getTime(int seconds) {
-    return getTime(Duration.ofSeconds(seconds));
   }
 
   /**
@@ -126,46 +53,36 @@ public class TimeUtil {
    * @return formatted time
    */
   public static String getTime(final Duration duration) {
-    final StringBuilder builder = new StringBuilder();
+    return getTime(duration.getSeconds());
+  }
 
-    long seconds = duration.getSeconds();
+  public static String getTime(long seconds) {
+    final StringJoiner joiner = new StringJoiner(" ");
+
     long minutes = seconds / 60;
     long hours = minutes / 60;
-    long days = hours / 24;
+    final long days = hours / 24;
 
     seconds %= 60;
     minutes %= 60;
-    hours %= 60;
-    days %= 24;
+    hours %= 24;
 
-    if (seconds > 0) {
-      builder.insert(0, seconds + "s");
-    }
-
-    if (minutes > 0) {
-      if (builder.length() > 0) {
-        builder.insert(0, ' ');
-      }
-
-      builder.insert(0, minutes + "m");
+    if (days > 0) {
+      joiner.add(days + "d");
     }
 
     if (hours > 0) {
-      if (builder.length() > 0) {
-        builder.insert(0, ' ');
-      }
-
-      builder.insert(0, hours + "h");
+      joiner.add(hours + "h");
     }
 
-    if (days > 0) {
-      if (builder.length() > 0) {
-        builder.insert(0, ' ');
-      }
-
-      builder.insert(0, days + "d");
+    if (minutes > 0) {
+      joiner.add(minutes + "m");
     }
 
-    return builder.toString();
+    if (seconds > 0) {
+      joiner.add(seconds + "s");
+    }
+
+    return joiner.toString();
   }
 }
