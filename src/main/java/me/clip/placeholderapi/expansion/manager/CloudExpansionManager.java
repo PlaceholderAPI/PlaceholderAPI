84948101f46bf3eebafd125d554bd1b0b4e93f41
PiggyPiglet <noreply@piggypiglet.me>
2026-01-12 17:00:33 +0800

    Update API URL

--- src/main/java/me/clip/placeholderapi/expansion/manager/CloudExpansionManager.java
+++ src/main/java/me/clip/placeholderapi/expansion/manager/CloudExpansionManager.java
@@ -60,7 +60,7 @@ import org.jetbrains.annotations.Unmodifiable;
 public final class CloudExpansionManager {
 
   @NotNull
-  private static final String API_URL = "http://api.extendedclip.com/v2/";
+  private static final String API_URL = "https://ecloud.placeholderapi.com/v2/";
 
   @NotNull
   private static final Gson GSON = new Gson();
