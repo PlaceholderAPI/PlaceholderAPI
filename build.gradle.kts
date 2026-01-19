880ddc22b5c73d69ad3d621eaec3cb78034c5852
PiggyPiglet <noreply@piggypiglet.me>
2026-01-19 17:20:13 +0800

    bump version to 2.12.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "me.clip"
-version = "2.11.8-DEV-${System.getProperty("BUILD_NUMBER")}"
+version = "2.12.0-DEV-${System.getProperty("BUILD_NUMBER")}"
 
 description = "An awesome placeholder provider!"
 
